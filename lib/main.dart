87a56cb2b05bb81ea8db16270d35e5b9cd344286
Assassin33GT <bodeidris33@gmail.com>
2025-04-18 13:55:17 +0200

    Describe your changes here

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 import 'package:random_quote_generator/start_page.dart';
 
 void main() {
-  runApp(const StartPage()); //roh nam
+  runApp(const StartPage());
 }
